4405b4da4787ecf8e9a16d38d28aa3a2b84f977d
Lucky Pratama <luckypratama71@yahoo.com>
2018-05-19 23:27:23 +0700

    Adding example

--- src/main/java/org/example/java_hbase/example1/UpdateData.java
+++ src/main/java/org/example/java_hbase/example1/UpdateData.java
@@ -1,5 +1,54 @@
 package org.example.java_hbase.example1;
 
-public class UpdateData {
+import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class UpdateData {
+	public static void main (String[] args) {
+		
+		try {
+		// Instantiating Configuration class
+		Configuration config = HBaseConfiguration.create();
+		
+		// Instantiating Connection class
+		Connection connection = ConnectionFactory.createConnection(config);
+		
+		// Instantiating Table class
+		Table table = connection.getTable(TableName.valueOf("employee"));
+		
+		// Instantiating Put class
+		// Accepts a row name
+		Put p = new Put(Bytes.toBytes("r2"));
+			try {
+				// Updating values using addColumn() method.
+				// The method pass three arguments i.e. column family, qualifier, and value
+				p.addColumn(Bytes.toBytes("personal data"),
+						Bytes.toBytes("name"),Bytes.toBytes("Pratama Lucky"));
+				
+				p.addColumn(Bytes.toBytes("personal data"),
+						Bytes.toBytes("nick_name"), Bytes.toBytes("Pratama"));
+				
+				p.addColumn(Bytes.toBytes("personal data"), 
+						Bytes.toBytes("address"), Bytes.toBytes("Jakarta"));
+			
+				// Update data
+				table.put(p);
+				System.out.println("Data updated successfully");
+				
+			} finally {
+				table.close();
+				connection.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
